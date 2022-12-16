2318c421b7ae6da89b2531fd3d93a0963c934e9b
Kostas Tsolkas <ktsolkas0@gmail.com>
2022-12-16 06:53:20 +0200

    Added functionality so that any requests that are not part of the API, return the React app so it can handle routing

--- server/index.js
+++ server/index.js
@@ -1,31 +1,63 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path, { dirname } from 'path';
+import cluster from 'cluster';
+import os from 'os';
+import { fileURLToPath } from 'url';
 
-import watchlistRoutes from "./routes/watchlist.js";
-import userRoutes from "./routes/user.js";
-import reviewRoutes from "./routes/review.js";
+import watchlistRoutes from './routes/watchlist.js';
+import userRoutes from './routes/user.js';
+import reviewRoutes from './routes/review.js';
 
-const app = express();
-
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-
-app.use(cors());
-app.use("/watchlist", watchlistRoutes);
-app.use("/user", userRoutes);
-app.use("/review", reviewRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://usernamekt:usernamekt123@cluster0.vpi3s.mongodb.net/?retryWrites=true&w=majority";
+const numCPUs = os.cpus().length;
+const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
-  .catch((error) => console.log(error.message));
+if (!isDev && cluster.isPrimary) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-app.get("/", (req, res) => {
-  res.cookie("cookieName", "cookieValue", { sameSite: "none", secure: true });
-  res.send("APP IS RUNNING.");
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.use(express.json({ limit: '30mb', extended: true }));
+  app.use(express.urlencoded({ limit: '30mb', extended: true }));
+
+  app.use(cors());
+  app.use('/watchlist', watchlistRoutes);
+  app.use('/user', userRoutes);
+  app.use('/review', reviewRoutes);
+
+  const CONNECTION_URL =
+    'mongodb+srv://usernamekt:usernamekt123@cluster0.vpi3s.mongodb.net/?retryWrites=true&w=majority';
+
+  mongoose
+    .connect(CONNECTION_URL)
+    .then(() =>
+      app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
+
+  app.get('/', (req, res) => {
+    res.cookie('cookieName', 'cookieValue', { sameSite: 'none', secure: true });
+    res.send('APP IS RUNNING.');
+  });
+
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
